6a9d7c87cf1d38e15038bbdc1126c5ba0793d905
shiroi36 <keita104824@gmail.com>
2023-03-17 12:49:41 +0900

    2023年3月17日

--- command.bat
+++ command.bat
@@ -1,5 +1,5 @@
 git init
-git remote add git@github.com:shiroi36/00WorkingTemplate.git
+git remote add origin git@github.com:shiroi36/00WorkingTemplate.git
 git add .
 git commit -m 2023”N3ŒŽ17“ú
 git push origin master
